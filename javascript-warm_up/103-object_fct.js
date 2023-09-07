b8a5e8ab474565ec1ef303930f8350801e9e5464
martinsibuchim <martinsibuchim@gmail.com>
2023-09-07 03:26:59 +0100

    Increment object

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -2,19 +2,21 @@
 
 const myObject = {
     type: 'object',
-    value: 12,
-    incr: function () {
-      this.value++;
-    }
+    value: 12
   };
-
-console.log(myObject);
-
-myObject.incr();
-console.log(myObject);
-
-myObject.incr();
-console.log(myObject);
-
-myObject.incr();
-console.log(myObject);
+  
+  console.log(myObject);
+  
+  myObject.incr = function () {
+    this.value++;
+  };
+  
+  myObject.incr();
+  console.log(myObject);
+  
+  myObject.incr();
+  console.log(myObject);
+  
+  myObject.incr();
+  console.log(myObject);
+  
