a8a04ae0db81a1631ecf4dc9ca9c9bf09e12cbd2
martinsibuchim <martinsibuchim@gmail.com>
2023-09-16 23:11:37 +0100

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,13 +2,12 @@
 /* class Rectangle that defines a rectangle*/
 class Rectangle {
     constructor(w, h) {
-      if (w <= 0 || h <= 0) {
-        // If either width or height is non-positive, create an empty object.
-        return 'Rectangle {}';
-      }else
-      // Initialize the instance attributes width and height with the provided values.
-      this.width = w;
-      this.height = h;
+        if (w <= 0 || h <= 0) {
+          return {};
+        }
+    
+        this.width = w;
+        this.height = h;
     }
 }
 
