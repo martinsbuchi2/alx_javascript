9a1305cd0f95e3f557f3e6dc46f552f07aa21325
martinsibuchim <martinsibuchim@gmail.com>
2023-09-16 19:20:42 +0100

    Rectangle #1

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* class Rectangle that defines a rectangle*/
 class Rectangle {
-    rectangle(w, h) {
+    constructor(w, h) {
         this.height = h;
         this.width = w;
     };
