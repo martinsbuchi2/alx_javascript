a265417545c12b90662ed563c395481399115115
martinsibuchim <martinsibuchim@gmail.com>
2023-09-16 22:38:36 +0100

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -5,8 +5,7 @@ class Rectangle {
       if (w <= 0 || h <= 0) {
         // If either width or height is non-positive, create an empty object.
         return 'Rectangle {}';
-      }
-  
+      }else
       // Initialize the instance attributes width and height with the provided values.
       this.width = w;
       this.height = h;
