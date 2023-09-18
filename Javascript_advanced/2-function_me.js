e0de367c4d641b5e9f0fe7b985788f79aecd3f33
martinsibuchim <martinsibuchim@gmail.com>
2023-09-18 17:44:43 +0100

    Closure

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,14 @@
+#!usr/bin/node 
+function welcomeMessage (fullName){
+    return function () {
+        alert(`Welcome ${fullName}`);
+    };
+}
+
+let guillaume = welcomeMessage('Guillaume');
+let alex = welcomeMessage('Alex');
+let fred = welcomeMessage('Fred');
+
+guillaume();
+alex();
+fred();
